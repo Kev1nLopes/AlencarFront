549eecac709d5220f7aba316c2d5d53e5eef7033
Kevin Lopes <contatokevinlopes@gmail.com>
2023-11-16 20:24:14 -0300

    Adicionei o interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routes';
 import { ProductsComponent } from './pages/products/products.component';
@@ -11,6 +11,7 @@ import { ClientsComponent } from './pages/clients/clients.component';
 import { VehicleComponent } from './pages/vehicle/vehicle.component';
 import { ShippingCompanyComponent } from './pages/shipping-company/shipping-company.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { JwtAuthInterceptor } from './interceptor/jwt-interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { RegisterComponent } from './pages/auth/register/register.component';
     RouterModule.forRoot(AppRoutes),
   
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtAuthInterceptor, multi: true} //Multi significa que podemos ter multiplos objetos utilizando esse jwt
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptor/jwt-interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AuthService } from '../services/auth.service';
+import { Observable } from "rxjs";
+import { Environment } from "../environments/environment";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class JwtAuthInterceptor implements HttpInterceptor{
+
+    constructor(private auth: AuthService){
+
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const isLoggedIn = this.auth.isLogged;
+        const isToServer = req.url.startsWith(Environment.baseUrl);
+
+        if(isLoggedIn && isToServer){
+            req = req.clone({
+                setHeaders: {Authorization: `Bearer ${this.auth.jwt}`}
+            })
+        }
+        return next.handle(req);
+    }
+}
